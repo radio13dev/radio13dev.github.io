b7ef0360d47d515920e72c6e3dc13c0c8cc9390c
Ben Wheatley <radio13dev@gmail.com>
2025-12-01 16:38:12 +1000

    Click throughs + better transitions + loader improvments

--- public/resources/js/resize.js
+++ public/resources/js/resize.js
@@ -17,6 +17,12 @@ function adjustSVGPositions(forceUpdate = false) {
         return;
     }
     isPortrait = newIsPortrait;
+    isHidingEverything = false;
+    if (gameLoadState && gameLoadState == 4)
+    {
+        // We actually gotta transition to the 'hide absolutely everything' state.
+        isHidingEverything = true;
+    }
     
     // Right Bottom hugs
     {
@@ -35,25 +41,25 @@ function adjustSVGPositions(forceUpdate = false) {
         //gsap.to('text.edge-hug-right-bottom', {x: targetCX, y: targetCY, duration: 0.6, ease: "power2.out"});
     }
 
-    // Right Bottom hugs 2
-    {
-        var targetLeft, targetTop, targetRight, targetBottom;
-        if (isPortrait) {
-            // More vertical layout
-            targetLeft = "0%";
-            targetRight = "0%"
-            targetTop = "40vmin";
-            targetBottom = "0%";
-        } else {
-            // More horizontal layout
-            targetLeft = "40vmin";
-            targetRight = "0%"
-            targetTop = "0%";
-            targetBottom = "0%";
-        }
-        gsap.killTweensOf('.area-hug-right-bottom');
-        gsap.to('.area-hug-right-bottom', {left:targetLeft, right:targetRight, top:targetTop, bottom:targetBottom, duration: 0.6, ease: "power2.out"});
-    }
+    //// Right Bottom hugs 2
+    //{
+    //    var targetLeft, targetTop, targetRight, targetBottom;
+    //    if (isPortrait) {
+    //        // More vertical layout
+    //        targetLeft = "0%";
+    //        targetRight = "0%"
+    //        targetTop = "40vmin";
+    //        targetBottom = "0%";
+    //    } else {
+    //        // More horizontal layout
+    //        targetLeft = "40vmin";
+    //        targetRight = "0%"
+    //        targetTop = "0%";
+    //        targetBottom = "0%";
+    //    }
+    //    gsap.killTweensOf('.area-hug-right-bottom');
+    //    gsap.to('.area-hug-right-bottom', {left:targetLeft, right:targetRight, top:targetTop, bottom:targetBottom, duration: 0.6, ease: "power2.out"});
+    //}
 
     // Left Top hugs
     {
@@ -64,45 +70,67 @@ function adjustSVGPositions(forceUpdate = false) {
             targetRight = "0%"
             targetTop = "0%";
             targetBottom = "60vmin";
+            if (isHidingEverything)
+            {
+                targetLeft = "0%";
+                targetRight = "0%"
+                targetTop = "-60vmin";
+                targetBottom = "100%";
+            }
         } else {
             // More horizontal layout
             targetLeft = "0%";
             targetRight = "60vmin"
             targetTop = "0%";
             targetBottom = "0%";
+            if (isHidingEverything)
+            {
+            targetLeft = "-60vmin";
+            targetRight = "100%"
+            targetTop = "0%";
+            targetBottom = "0%";
+            }
         }
         gsap.killTweensOf('.area-hug-left-top');
         gsap.to('.area-hug-left-top', {left:targetLeft, right:targetRight, top:targetTop, bottom:targetBottom, duration: 0.6, ease: "power2.out"});
     }
 
-    // Left Top hugs
-    {
-        var targetCX, targetCY;
-        if (isPortrait) {
-            // More vertical layout
-            targetCX = "50%";
-            targetCY = "0%";
-        } else {
-            // More horizontal layout
-            targetCX = "0%";
-            targetCY = "50%";
-        }
-        gsap.killTweensOf('.edge-hug-left-top');
-        gsap.to('div.edge-hug-left-top', {left: targetCX, top: targetCY, duration: 0.6, ease: "power2.out"});
-    }
+    //// Left Top hugs
+    //{
+    //    var targetCX, targetCY;
+    //    if (isPortrait) {
+    //        // More vertical layout
+    //        targetCX = "50%";
+    //        targetCY = "0%";
+    //    } else {
+    //        // More horizontal layout
+    //        targetCX = "0%";
+    //        targetCY = "50%";
+    //    }
+    //    gsap.killTweensOf('.edge-hug-left-top');
+    //    gsap.to('div.edge-hug-left-top', {left: targetCX, top: targetCY, duration: 0.6, ease: "power2.out"});
+    //}
 
     // Left Top hugs
     {
-        var targetTransform;
-        if (isPortrait) {
-            // More vertical layout
-            targetTransform = "matrix(0,1,-1,0,0,0)";
-        } else {
-            // More horizontal layout
-            targetTransform = "matrix(1,0,0,1,0,0)";
+        if (loadingCover)
+        {
+            var rotation;
+            if (isPortrait) {
+                // More vertical layout
+                rotation = 270;
+            } else {
+                // More horizontal layout
+                rotation = 180;
+            }
+            if (loadingCover.style.display == "")
+            {
+                // Add 180 degrees
+                rotation += 180;
+            }
         }
-        gsap.killTweensOf('.rotate-hug-right-bottom');
-        gsap.to('.rotate-hug-right-bottom', {transform:targetTransform, duration: 0.6, ease: "power2.out"});
+        gsap.killTweensOf(loadingCover);
+        gsap.to(loadingCover, {transformOrigin: "70 50%", rotation: rotation, duration: 0.6, ease: "power2.out"});
     }
 
     // Update all the scale-with-vmin elements
